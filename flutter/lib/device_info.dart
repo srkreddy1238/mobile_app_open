152d22bc936cc69bdb8b351569b316455c9adf33
Danil Uzlov <DanilUzlov@yandex.ru>
2022-09-08 09:44:44 +0700

    remove ios_utsname_ext dependency

--- flutter/lib/device_info.dart
+++ flutter/lib/device_info.dart
@@ -4,7 +4,6 @@ import 'package:flutter/services.dart';
 
 import 'package:device_info/device_info.dart';
 import 'package:device_marketing_names/device_marketing_names.dart';
-import 'package:ios_utsname_ext/extension.dart';
 import 'package:mlperfbench_common/data/environment/environment_info.dart';
 import 'package:mlperfbench_common/data/environment/os_enum.dart';
 
@@ -52,23 +51,27 @@ class DeviceInfo {
   static Future<DeviceInfo> _makeIosInfo() async {
     final deviceInfo = DeviceInfoPlugin();
     final iosInfo = await deviceInfo.iosInfo;
+    final deviceNames = DeviceMarketingNames();
 
     return DeviceInfo(
       manufacturer: 'Apple',
       brand: 'Apple',
       modelCode: iosInfo.utsname.machine,
-      modelName: iosInfo.utsname.machine.iOSProductName,
+      modelName: deviceNames.getSingleNameFromModel(
+          DeviceType.ios, iosInfo.utsname.machine),
     );
   }
 
   static Future<DeviceInfo> _makeAndroidInfo() async {
     final deviceInfo = await DeviceInfoPlugin().androidInfo;
     final deviceNames = DeviceMarketingNames();
+
     return DeviceInfo(
       manufacturer: deviceInfo.manufacturer,
       brand: deviceInfo.brand,
       modelCode: deviceInfo.model,
-      modelName: await deviceNames.getSingleName(),
+      modelName: deviceNames.getSingleNameFromModel(
+          DeviceType.android, deviceInfo.model),
     );
   }
 
